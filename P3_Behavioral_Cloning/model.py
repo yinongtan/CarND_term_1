ba73eb9eb9b44318eb61098633ebc2248bf8812e
yinongtan <yinong@umich.edu>
2017-09-16 22:33:40 -0500

    p3_test_3

--- P3_Behavioral_Cloning/model.py
+++ P3_Behavioral_Cloning/model.py
@@ -37,7 +37,7 @@ def generator(samples, batch_size=32):
 
                 center_angle = float(batch_sample[3])
                 # create adjusted steering measurements for the side camera images
-                correction = 0.5 # this is a parameter to tune
+                correction = 0.65 # this is a parameter to tune
                 left_angle = center_angle + correction
                 right_angle = center_angle - correction
 
@@ -63,8 +63,8 @@ validation_generator = generator(validation_samples, batch_size=32)
 
 row, col, ch = 160, 320, 3  # Trimmed image format
 
-from keras.models import Sequential
-from keras.layers import Flatten, Dense, Lambda, Cropping2D
+from keras.models import Sequential, Model
+from keras.layers import Flatten, Dense, Lambda, Cropping2D, Dropout
 from keras.layers.convolutional import Convolution2D
 from keras.layers.pooling import MaxPooling2D
 
@@ -78,13 +78,33 @@ model.add(Convolution2D(48,5,5,subsample=(2,2), activation = "relu"))
 model.add(Convolution2D(64,3,3, activation = "relu"))
 model.add(Convolution2D(64,3,3, activation = "relu"))
 model.add(Flatten())
+model.add(Dropout(0.5))
 model.add(Dense(100))
+model.add(Dropout(0.5))
 model.add(Dense(50))
 model.add(Dense(1))
 
+import matplotlib
+matplotlib.use('agg')
+import matplotlib.pyplot as plt
+
 model.compile(loss='mse', optimizer='adam')
-model.fit_generator(train_generator, samples_per_epoch= \
+history_object = model.fit_generator(train_generator, samples_per_epoch= \
             len(6 * train_samples), validation_data=validation_generator, \
-            nb_val_samples=len(6 * validation_samples), nb_epoch=3)
+            nb_val_samples=len(6 * validation_samples), nb_epoch=1, verbose=1)
 
 model.save('model.h5')
+
+# print the keys contained in the history object
+print(history_object.history.keys())
+
+# plot the training and validation loss for each epoch
+plt.plot(history_object.history['loss'])
+plt.plot(history_object.history['val_loss'])
+plt.title('model mean squared error loss')
+plt.ylabel('mean squared error loss')
+plt.xlabel('epoch')
+plt.legend(['training set', 'validation set'], loc='upper right')
+plt.savefig('MSE_history.png')
+
+
